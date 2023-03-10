efec02881a4982dc789a144545e68dfdb81484d7
jcanaviri42 <jcanaviri42@gmail.com>
2023-03-10 00:12:02 -0400

    add condititals examples and files examples in bash

---
+++ conditions.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+age=16
+
+if [ $age -ge 18 ]
+then 
+    echo "You are an adult"
+else
+    echo "access denied"
+fi
+
+if [ $age -eq 16 ]
+then
+    echo "Happy sweet 16 🥳"
+fi
---
+++ files.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+: 'This is a multiline comment
+this is the second line'
+
+# This code will replace all the content in hello.txt
+echo 'Hello, world!' > hello.txt
+
+# But this code will append the content
+echo 'Hello, world!' >> hello.txt
