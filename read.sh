6b2eb1223b11d08f9c19883796a85bd5c685acc2
jcanaviri42 <jcanaviri42@gmail.com>
2023-03-12 22:17:58 -0400

    Add read.sh example of reading in bash

---
+++ read.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+: 'The two symbols #! is pronunced as "shee-bang"
+and that expresion should be in the header of every bash-scripting file
+'
+
+echo -n "Please tell me your age: "
+read age
+
+echo "I see 🤔, you are $age years old."
