cf78d9a6a8549a0d3916c581e2f7a704f981482d
jcanaviri42 <jcanaviri42@gmail.com>
2023-03-14 00:07:16 -0400

    Add more for loops examples in bash

--- for_loop.sh
+++ for_loop.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
+
+# Iterating over a list of numbers.
+echo "Numbers from 1 to 5: "
+for i in 1 2 3 4 5; do
+    echo -n "$i "
+done
+
+echo ""
+
+
+# Iterating over a list of numbers.
+echo "Numbers from 1 to 10: "
+for i in {0..10}; do
+    echo -n "$i "
+done
+
+echo ""
+
+
+# A more programatic way to iterate over numbers.
 echo "Numbers from 1 to 10: "
 
 for (( i=0; i < 10; i++ )); do
@@ -8,6 +28,8 @@ done
 
 echo ""
 
+
+# continue keyword
 echo "Odd numbers from 1 to 10: "
 for (( i=0; i < 10; i++ )); do
     if [ $((i % 2)) -eq 0 ]; then
@@ -17,3 +39,14 @@ for (( i=0; i < 10; i++ )); do
 done
 
 echo ""
+
+
+# break keyword
+echo "Numbers from 1 to 10 with a break statement: "
+for (( i=0; i < 10; i++ )); do
+    # if the number is not equals to 0 and if it is multiple of 7
+    if [[ $i -ne 0 && $((i % 7)) -eq 0 ]]; then
+        break
+    fi
+    echo -n "$i "
+done
