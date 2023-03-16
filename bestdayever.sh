eeac281d342453e65c39fc96f263e41ae9eba5b9
jcanaviri42 <jcanaviri42@gmail.com>
2023-03-15 22:09:02 -0400

    better implementation of the program bestdayever.sh

--- bestdayever.sh
+++ bestdayever.sh
@@ -5,11 +5,19 @@ user=$(whoami)
 date=$(date)
 workdir=$(pwd)
 
-echo "Good morning $name!!! 👌"
-sleep 1
-echo "Your looking good today $name!!! 👍"
-sleep 1
-echo "You have the best $attribute I've ever seen $name!!! 🫵"
-sleep 2
-
-echo "You are currently logged in as $user, and you are at the directory $workdir. Also today is $date"
+if [[ $name == '' || $attribute == '' ]]; then
+    echo "You didn't provide your name or your attribute"
+    echo "You must execute this program like this: ./bestdayever.sh YourName YourFavoriteAttribute"
+else
+    echo "Good morning $name!!! 👌"
+    sleep 1
+    echo "Your looking good today $name!!! 👍"
+    sleep 1
+    echo "You have the best $attribute I've ever seen $name!!! 🫵"
+    sleep 2
+    
+    echo "You are currently logged in as $user, 
+    and you are at the directory $workdir. 
+    Also today is $date"
+    
+fi
